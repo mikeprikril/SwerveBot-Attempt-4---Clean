c05ae5947b6ec976cfc8e7cbcb86df32d3337e25
Mike <mprikril@gmail.com>
2023-12-15 12:01:36 -0500

    Remove LED values

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -212,28 +212,5 @@ public final class Constants {
 
   public static final class LEDConstants {
     public static final int blinkinPort = 0;
-    public static final int blinkPerSecond = 50 / 5;
-    public static final double forestPattern = -0.37;
-    public static final double colorGradient = 0.41;
-    public static final double solidYellow = 0.69;
-    public static final double solidViolet = 0.91;
-    public static final double solidGreen = 0.77;
-    public static final double solidRed = 0.61;
-    public static final double solidBlue = 0.87;
-    public static final double solidOrange = 0.65;
-    public static final double skyblue = 0.83;
-    public static final double off = 0.99;
-    public static final double rainbow = -0.99;
-    public static final double hotPink = 0.57;
-    public static final double darkRed = 0.59;
-    public static final double red = 0.61;
-    public static final double redOrange = 0.63;
-    public static final double orange = 0.65;
-    public static final double gold = 0.67;
-    public static final double yellow = 0.69;
-    public static final double lawnGreen = 0.71;
-    public static final double lime = 0.73;
-    public static final double darkGreen = 0.75;
-    public static final double green = 0.77;
   }
 }
